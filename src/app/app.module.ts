cab588b4da203bb6278af773d9637a8f46f0f04d
caldwellusmc <deshanecaldwell@yahoo.com>
2019-01-03 10:11:38 -0500

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {MatToolbarModule} from '@angular/material';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { HeaderComponent } from './components/header/header.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -14,6 +17,7 @@ import { HeaderComponent } from './components/header/header.component';
   ],
   imports: [
     BrowserModule,
+    MatToolbarModule,
     AppRoutingModule
   ],
   providers: [],
