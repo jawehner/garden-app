005aeb9f2db8f7e238dd8c0b57365c4ed0dcd64e
jawehner <jawehner@gmail.com>
2019-01-03 10:51:40 -0500

    little changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+// import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
+import { RouterModule } from '@angular/router'
+
+const routes = [
+  { path: 'products', component: ProductsComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    // AboutComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
